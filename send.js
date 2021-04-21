1be2278222127f546681fe1ce21da3d293fc35db
rfoldbirk <rasmusfoldberg@gmail.com>
2021-04-21 12:36:27 +0200

    xdotool backspace + return

--- send.js
+++ send.js
@@ -2,4 +2,6 @@ const { execSync } = require('child_process')
 let string = process.argv[2].split('').join(' ')
 string.replace(' : d ', ' 0xff08 ')
 string.replace(' : r ', ' 0xff0d ')
+
+console.log(string)
 execSync(`DISPLAY=:0 xdotool key ${ string }`)
