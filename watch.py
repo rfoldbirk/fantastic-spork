a61ec8e896b07575083d1bf0bf9c2421ba5fe12c
rfoldbirk <rasmusfoldberg@gmail.com>
2021-04-21 13:13:32 +0200

    pg

--- watch.py
+++ watch.py
@@ -9,15 +9,21 @@ import json
 
 # initialize the camera and grab a reference to the raw camera capture
 camera = PiCamera()
-rawCapture = PiRGBArray(camera)
-time.sleep(0.2) # allow the camera to warmup
-
-# cv.waitKey(0)
-
-
-
-for i in range(10):
-	camera.capture(rawCapture, format="bgr")
-	image = rawCapture.array
-	cv.imshow("Image", image)
-	time.sleep(2)
+camera.resolution = (640, 480)
+camera.framerate = 32
+rawCapture = PiRGBArray(camera, size=(640, 480))
+# allow the camera to warmup
+time.sleep(0.1)
+# capture frames from the camera
+for frame in camera.capture_continuous(rawCapture, format="bgr", use_video_port=True):
+	# grab the raw NumPy array representing the image, then initialize the timestamp
+	# and occupied/unoccupied text
+	image = frame.array
+	# show the frame
+	cv.imshow("Frame", image)
+	key = cv.waitKey(1) & 0xFF
+	# clear the stream in preparation for the next frame
+	rawCapture.truncate(0)
+	# if the `q` key was pressed, break from the loop
+	if key == ord("q"):
+		break
