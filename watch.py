14e8987d1a68b97aedacf5b7b588cb2b04887aa1
rfoldbirk <rasmusfoldberg@gmail.com>
2021-04-21 12:54:13 +0200

    pg

--- watch.py
+++ watch.py
@@ -17,3 +17,13 @@ image = rawCapture.array
 # display the image on screen and wait for a keypress
 cv2.imshow("Image", image)
 cv2.waitKey(0)
+
+
+while True:
+	camera.capture(rawCapture, format="bgr")
+	image = rawCapture.array
+
+	# display the image on screen and wait for a keypress
+	cv2.imshow("Image", image)
+
+	time.sleep(2)
