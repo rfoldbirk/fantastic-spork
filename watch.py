f0af0bd2771769affe6b3f7c19ebbb8343d91c40
rfoldbirk <rasmusfoldberg@gmail.com>
2021-04-21 13:25:10 +0200

    pg

--- watch.py
+++ watch.py
@@ -11,6 +11,35 @@ import json
 model = cv2.CascadeClassifier('training/cascade.xml')
 
 
+def findAvgBest(detected):
+	# Beregner gennemsnittet
+	avg = 0
+	for score in detected[1]:
+		avg += int( score[0] )
+	avg = avg / len(detected[1])
+
+	# Overskriver gennemsnittet hvis det er specificeret
+	if average_confidence:
+		avg = average_confidence
+	
+	# Vælger 
+	selected_indexes = []
+	index = 0
+	for score in detected[1]:
+		if int( score[0] ) > avg:
+			selected_indexes.append(index)
+
+		index += 1
+
+	selected_rects = []
+	for i in selected_indexes:
+		selected_rects.append({
+			"rect": jd( detected[0][i] ),
+			"confidence": jd( detected[1][i][0] )
+		})
+
+	return selected_rects
+
 # initialize the camera and grab a reference to the raw camera capture
 camera = PiCamera()
 camera.resolution = (640, 480)
@@ -27,7 +56,16 @@ for frame in camera.capture_continuous(rawCapture, format="bgr", use_video_port=
 	image = frame.array
 
 	# Detection
-	image = cv2.rectangle(image, (10, 10), (50, 50), (255, 0, 0), 2)
+	dms = model.detectMultiScale2(image)
+	best_rectangles = findAvgBest(dms)
+
+	for elem in best:
+		bp = (elem.rect[0], elem.rect[1])
+		ep = (elem.rect[2], elem.rect[3])
+		print(bp, ep)
+		image = cv2.rectangle(image, bp, ep, (255, 0, 0), 2)
+	
+
 	# show the frame
 	cv2.imshow("Frame", image)
 	key = cv2.waitKey(1) & 0xFF
